2f030292746ac5ee5ead08a3825abbdf96dc5d9a
krets <krets.anastasiia@gmail.com>
2025-07-08 12:09:30 -0700

    second commit

--- lesson2/lesson2task1.py
+++ lesson2/lesson2task1.py
@@ -1,11 +1,12 @@
 from unicodedata import digit
 
-number = int (input("Enter a 4-digit numbers:"))
+number = int (input("Enter a 5-digit numbers:"))
 digit1 = number % 10
 digit2 = (number // 10 ) % 10
 digit3 = (number // 100 ) % 10
-digit4 = (number //1000 ) % 10
-reserved_number = digit1 * 1000 + digit2 * 100 + digit3 * 10 + digit4
+digit4 = (number // 1000 ) % 10
+digit5 = (number // 10000) % 10
+reserved_number = digit1 * 10000 + digit2 * 1000 + digit3 * 100 + digit4 * 10 + digit5
 print("reversed number:", reserved_number)
 
 
